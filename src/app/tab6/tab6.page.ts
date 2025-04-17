6a561a37c5746bb2eabd5f4f4b15ea3b8622084a
goldshoot0720 <goldshoot0720@gmail.com>
2025-04-17 21:59:57 +0800

    ionic_example_tab6_feng2025_feng37 First

---
+++ src/app/tab6/tab6.page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab6',
+  templateUrl: 'tab6.page.html',
+  styleUrls: ['tab6.page.scss'],
+  standalone: false,
+})
+export class Tab6Page {
+
+  constructor() {}
+
+}
